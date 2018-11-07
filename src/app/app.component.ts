8b28c67c085f291b14e150b9ff27cd1107fe9acd
Benedikt Werner <1benediktwerner@gmail.com>
2018-11-07 06:37:45 +0100

    Add service worker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'money-sync';
+
+  update = false;
+
+  constructor(updates: SwUpdate) {
+    updates.available.subscribe(event => {
+      this.update = true;
+    });
+  }
 }
