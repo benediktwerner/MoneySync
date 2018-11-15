7aab2b33a4f7a633064f51df9b83c10a3d48c51d
Benedikt Werner <1benediktwerner@gmail.com>
2018-11-15 17:35:53 +0100

    Add basic charts

--- src/app/overview/overview.component.ts
+++ src/app/overview/overview.component.ts
@@ -1,23 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { formatDate } from '@angular/common';
 import { Observable } from 'rxjs';
 import { DataService, Account, Transaction } from '../data.service';
 import { map } from 'rxjs/operators';
 import { MatDialog } from '@angular/material';
 import { TransactionDialogComponent } from '../transaction-dialog/transaction-dialog.component';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-overview',
   templateUrl: './overview.component.html',
   styleUrls: ['./overview.component.scss'],
 })
-export class OverviewComponent {
+export class OverviewComponent implements OnInit {
   accounts: Observable<Account[]>;
   transactions: Observable<{ key: string; elements: Transaction[] }[]>;
 
   totalBalance: Observable<number>;
   currentMonthTotal: Observable<number>;
   lastMonthTotal: Observable<number>;
+
+  private chart;
 
   constructor(private dialog: MatDialog, data: DataService) {
     this.accounts = data.accounts;
@@ -42,9 +45,63 @@ export class OverviewComponent {
       })
     );
 
+    data.transactions.subscribe(transactions => {
+      let data = [];
+      let total = 0;
+
+      for (const trans of transactions.sort((a, b) => b.date.getTime() - a.date.getTime())) {
+        total += trans.amount;
+        data.push({
+          t: trans.date,
+          y: total,
+        });
+      }
+
+      this.ensureChart();
+      this.chart.data.datasets[0].data = data;
+      this.chart.update();
+    });
+
     this.totalBalance = data.totalBalance;
     this.currentMonthTotal = data.currentMonthTotal;
     this.lastMonthTotal = data.lastMonthTotal;
+  }
+
+  ngOnInit() {
+    this.ensureChart();
+  }
+
+  ensureChart() {
+    if (this.chart) return;
+    this.chart = new Chart(document.getElementById('chart'), {
+      type: 'line',
+      data: {
+        datasets: [
+          {
+            label: 'Total',
+            fill: false,
+            steppedLine: true,
+            backgroundColor: 'rgb(54, 162, 235)',
+            borderColor: 'rgb(54, 162, 235)',
+            data: [],
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+        tooltips: {
+          mode: 'index',
+          intersect: false,
+        },
+        scales: {
+          xAxes: [
+            {
+              type: 'time',
+            },
+          ],
+        },
+      },
+    });
   }
 
   showTransaction(transaction) {
