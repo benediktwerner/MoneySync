3763c882506a164b7a0e8ca467b6590cad3acd2d
Benedikt Werner <1benediktwerner@gmail.com>
2020-02-07 16:04:49 +0000

    Add firebase configuration to prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAXPGI_pP-aXUJhjTCAHi3R0bMPdHwJ4Wc",
+    authDomain: "money-sync.firebaseapp.com",
+    databaseURL: "https://money-sync.firebaseio.com",
+    projectId: "money-sync",
+    storageBucket: "money-sync.appspot.com",
+    messagingSenderId: "126122311270"
+  }
 };
